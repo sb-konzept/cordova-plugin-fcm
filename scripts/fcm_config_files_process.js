aee5ff0703683065860ee5cf71c0198608927b73
Michael Albertz <michael@prio.one>
2018-12-20 10:04:24 +0100

    fix invalid data chunk for android

--- scripts/fcm_config_files_process.js
+++ scripts/fcm_config_files_process.js
@@ -36,14 +36,15 @@ var PLATFORM = {
     },
     ANDROID: {
         dest: [
-            ANDROID_DIR + '/google-services.json'
+            ANDROID_DIR + '/google-services.json',
+            ANDROID_DIR + '/app/google-services.json',
         ],
         src: [
             'google-services.json',
             ANDROID_DIR + '/assets/www/google-services.json',
             'www/google-services.json'
         ],
-        stringsXml: ANDROID_DIR + '/res/values/strings.xml'
+        stringsXml: ANDROID_DIR + '/app/src/main/res/values/strings.xml'
     }
 };
 
